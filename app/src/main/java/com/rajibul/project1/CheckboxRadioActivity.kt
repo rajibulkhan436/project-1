d2b5af19bc5ad08c45c4779a787a523f822cc0df
rajibulkhan436 <138011628+rajibulkhan436@users.noreply.github.com>
2023-07-01 14:27:29 +0530

    2 pages done
    added checkbox,radio group and button

--- app/src/main/java/com/rajibul/project1/CheckboxRadioActivity.kt
+++ app/src/main/java/com/rajibul/project1/CheckboxRadioActivity.kt
@@ -3,16 +3,23 @@ package com.rajibul.project1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CheckBox
+import android.widget.Toast
 
 class CheckboxRadioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        var cbIAssure: CheckBox?=null
+        var cbIAssure: CheckBox? = null
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkbox)
-        cbIAssure=findViewById(R.id.cbIAssure)
 
-
+        cbIAssure = findViewById(R.id.cbIAssure)
+        cbIAssure?.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this, "Information verified", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Information not verified", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
